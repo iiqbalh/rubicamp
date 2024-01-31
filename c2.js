5f1b668df0cfb83b761ac4c9b320dc6f61ef4df3
iqbal <iqbalhernadi17@gmail.com>
2024-01-31 14:31:37 +0700

    merubah

--- c2.js
+++ c2.js
@@ -1,17 +1,18 @@
 function deretKaskus(n) {
     const arr = [];
 
-    for (let i = 1; i < n; i++) {
+    for (let i = 1; i <= n; i++) {
         let num = i * 3;
-        if (num % 5 === 0) {
-            arr.push("KAS")
+        if (num % 5 === 0 && num % 6 === 0) {
+            arr.push("KASKUS")
         } else if (num % 6 === 0){
             arr.push("KUS")
+        } else if (num % 5 === 0) {
+            arr.push("KAS")
         } else {
             arr.push(num)
         }
     }
-    arr.push("KASKUS")
     return arr
 }
 console.log(deretKaskus(10));
