0e252527242f2014912d89039cf1e00b872a3979
iqbal <iqbalhernadi17@gmail.com>
2024-02-19 14:45:24 +0700

    c11

--- c11.js
+++ c11.js
@@ -1,6 +1,6 @@
 const { readFileSync } = require('node:fs');
 const hasil = readFileSync('./data.json', 'utf-8');
-data = JSON.parse(hasil);
+const data = JSON.parse(hasil);
 
 
 const rl = require('readline').createInterface({
@@ -8,56 +8,84 @@ const rl = require('readline').createInterface({
     output: process.stdout,
 });
 
+
+let counter = 0;
+
 rl.question(`Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!\n
-Pertanyaan: ${data[0].definition}
+Pertanyaan: ${data[counter].definition}
 Tebakan: `, (txt) => {
-    if (txt === data[0].term) {
-        console.log('Selamat Anda Benar!')
+
+//jika jawaban benar
+    if (txt === data[0].term) { 
+        console.log('Selamat Anda Benar!');
+        counter++;
+
         rl.question(`\nPertanyaan: ${data[1].definition}
-Tebakan: `, (name) => {
-            if (name === data[1].term) {
-                console.log('Selamat Anda Benar!')
+Tebakan: `, (txt) => {
+            if (txt === data[1].term) {
+                console.log('Selamat Anda Benar!');
                 rl.close();
             } else {
-                console.log('Wkwkwk, Anda kurang beruntung!')
+                console.log('Wkwkwk, Anda kurang beruntung!');
+//-------------------------------------------------------------------
+
+                rl.question(`\nTebakan: `, (txt) => { //menebak ulang pertanyaan kedua
+                    if (txt === data[1].term) {
+                        console.log('Selamat Anda Benar!');
+                        rl.close();
+                    } else {
+                        console.log('Wkwkwk, Anda kurang beruntung!');
+                        rl.close();
+                    }
+                })
+            }
+        });
+
+//---------------------------------------------------------------------
+
+//jika jawaban salah
+    } else { 
+        console.log('Wkwkwk, Anda kurang beruntung!');
+
+        rl.question(`\nTebakan: `, (txt) => { //menebak ulang pertanyaan pertama
+            if (txt === data[counter].term) {
+                console.log('Selamat Anda Benar!');
+                counter++;
+//---------------------------------------------------------------------
+
+                rl.question(`\nPertanyaan: ${data[1].definition}
+Tebakan: `, (txt) => {                                          //lanjut ke pertanyaan kedua
+                    if (txt === data[1].term) {
+                        console.log('Selamat Anda Benar!');
+                        rl.close();
+                    } else {
+                        console.log('Wkwkwk, Anda kurang beruntung!');
+//---------------------------------------------------------------------
+
+                        rl.question(`\nTebakan: `, (txt) => {
+                            if (txt === data[1].term) {
+                                console.log('Selamat Anda Benar!');
+                                rl.close();
+                            } else {
+                                console.log('Wkwkwk, Anda kurang beruntung!');
+                                rl.close();
+                            }
+                        })
+                    }
+                });
+
+            } else {
+                console.log('Wkwkwk, Anda kurang beruntung!');
                 rl.close();
             }
         });
-    } else {
-        console.log('Wkwkwk, Anda kurang beruntung!')
     }
+
 });
 
 rl.on('close', () => {
-    console.log(`\nHore Anda Menang!\n`)
-})
+    console.log(`\nHore Anda Menang!\n`);
+});
 
-
-
-
-
-
-
-
-
-
-// const rl = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//     prompt: `Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!\n
-// Pertanyaan: ${data[0].definition}
-// Tebakan: `,
-// });
-
-// rl.prompt()
-
-// rl.on(`line`, (txt) => {
-//     if (txt === data[0].term) {
-//         console.log('Selamat Anda Benar!')
-//     } else {
-//         console.log('Wkwkwk, Anda kurang beruntung!')
-//     }
-//     process.exit(0)
-// });
 
 
