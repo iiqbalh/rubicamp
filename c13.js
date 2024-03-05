18f8359cab9016781a53b9fbbe943b01d5c3c3e4
iqbal <iqbalhernadi17@gmail.com>
2024-03-05 23:10:44 +0700

    setengah

--- c13.js
+++ c13.js
@@ -1,8 +1,32 @@
 const txt = process.argv[2];
+const { readFileSync, writeFileSync } = require('node:fs');
 
-if (!txt) {
-    console.log(`
->>> JS TODO <<<
+
+const reading = () => {
+    return JSON.parse(readFileSync('data2.json', 'utf-8'))
+}
+
+const writing = (value) => {
+    return JSON.parse(writeFileSync('data2.json', value, 'utf-8'))
+}
+
+const data = reading();
+
+switch (txt) {
+    case "list":
+        console.log(`daftar kerjaan:`)
+        data.forEach(item, index => {
+            console.log(`${index + 1}. [] ${item.title}`)
+        });
+        break;
+
+    case "add":
+        writing()
+        break;
+
+    default:
+        console.log(`
+ >>> JS TODO <<<
 $ node todo.js <command>
 $ node todo.js list
 $ node todo.js task <task_id>
@@ -14,6 +38,6 @@ $ node todo.js list:outstanding ascldesc
 $ node todo.js list:completed ascldesc
 $ node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
 $ node todo.js filter: <tag_name>
-`)
-process.exit();
+  `)
+        break;
 }
