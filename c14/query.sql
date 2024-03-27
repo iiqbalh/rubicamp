1c11210ffb2da99527128f43ff621cb9bafe45c9
iqbal <iqbalhernadi17@gmail.com>
2024-03-27 21:30:41 +0700

    c14

---
+++ c14/query.sql
@@ -0,0 +1,99 @@
+CREATE TABLE dosen (
+    id_dosen CHAR(2) PRIMARY KEY NOT NULL,
+    nama_dosen VARCHAR(50) NOT NULL
+);
+INSERT INTO dosen VALUES
+("01","abdul"),
+("02","malik"),
+("03","siti"),
+("04","ahmad"),
+("05","reni");
+
+CREATE TABLE jurusan (
+    id_jurusan CHAR(2) PRIMARY KEY NOT NULL,
+    nama_jurusan VARCHAR(50) NOT NULL
+);
+INSERT INTO jurusan VALUES
+("01","kedokteran"),
+("02","farmasi"),
+("03","psikologi"),
+("04","teknik komputer"),
+("05","teknik informatika");
+
+CREATE TABLE mahasiswa (
+    nim CHAR(4) PRIMARY KEY NOT NULL,
+    nama_mahasiswa VARCHAR(50) NOT NULL,
+    jurusan VARCHAR(50) NOT NULL,
+    alamat TEXT NOT NULL,
+    id_jurusan CHAR(2) NOT NULL,
+    id_matakuliah CHAR(3) NOT NULL,
+    id_dosen CHAR(2) NOT NULL,
+    FOREIGN KEY(id_jurusan) REFERENCES jurusan(id_jurusan)
+    FOREIGN KEY(id_matakuliah) REFERENCES matakuliah(id_matakuliah),
+    FOREIGN KEY(id_dosen) REFERENCES dosen(id_dosen)
+);
+INSERT INTO mahasiswa VALUES
+("0001","iqbal", "teknik informatika", "bandung", "05", "333", "03"),
+("0002","fauzi", "farmasi", "jakarta", "02", "111", "01"),
+("0003","yuri", "psikolog", "ciamis", "03", "444", "04"),
+("0004","cika", "kedokteran", "garut", "01", "555", "05"),
+("0005","ayu", "teknik komputer", "bandung", "04", "222", "02");
+
+CREATE TABLE  matakuliah (
+    id_matakuliah CHAR(3) PRIMARY KEY NOT NULL,
+    nama_matakuliah VARCHAR(50) NOT NULL,
+    sks VARCHAR(100) NOT NULL,
+    nim CHAR(4) NOT NULL,
+    id_dosen CHAR(2) NOT NULL,
+    FOREIGN KEY(nim) REFERENCES mahasiswa(nim),
+    FOREIGN KEY(id_dosen) REFERENCES dosen(id_dosen)
+);
+INSERT INTO matakuliah VALUES
+("111","analisis farmasi", "2", "0002", "01"),
+("222","aljabar linear", "2", "0005", "02"),
+("333","algoritma dan pemrograman", "2", "0001", "03"),
+("444","sosiologi", "1", "0003", "04"),
+("555","biologi kedokteran", "2", "0004", "05");
+
+CREATE TABLE mengajar (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_dosen CHAR(2) NOT NULL,
+    id_matakuliah CHAR(3) NOT NULL,
+    FOREIGN KEY(id_dosen) REFERENCES dosen(id_dosen),
+    FOREIGN KEY(id_matakuliah) REFERENCES matakuliah(id_matakuliah)
+);
+INSERT INTO mengajar (id_dosen, id_matakuliah) VALUES
+("01","111"),
+("02","222"),
+("03","333"),
+("04","444"),
+("05","555");
+
+CREATE TABLE membimbing (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_dosen CHAR(2) NOT NULL,
+    nim CHAR(3) NOT NULL,
+    FOREIGN KEY(id_dosen) REFERENCES dosen(id_dosen),
+    FOREIGN KEY(nim) REFERENCES mahasiswa(nim)
+);
+INSERT INTO membimbing (id_dosen, nim) VALUES
+("01","0002"),
+("02","0005"),
+("03","0001"),
+("04","0003"),
+("05","0004");
+
+CREATE TABLE mengikuti (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nim CHAR(4) NOT NULL,
+    id_matakuliah CHAR(3) NOT NULL,
+    nilai INTEGER DEFAULT 0,
+    FOREIGN KEY(nim) REFERENCES mahasiswa(nim),
+    FOREIGN KEY(id_matakuliah) REFERENCES matakuliah(id_matakuliah)
+);
+INSERT INTO mengikuti (nim, id_matakuliah) VALUES
+("0001","333"),
+("0002","111"),
+("0003","444"),
+("0004","555"),
+("0005","222");
