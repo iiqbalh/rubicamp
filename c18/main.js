4882f66576002d69dfcdea6614e49015c664bd10
iiqbalh <iqbalhernadi17@gmail.com>
2024-05-15 12:47:55 +0700

    commit

--- c18/main.js
+++ c18/main.js
@@ -1,1 +1,11 @@
-console.log('test')
+import sqlite3 from 'sqlite3'
+import path from 'path'
+
+const dbpath = path.join(path.resolve(), 'db', 'university.db')
+
+export const db = new sqlite3.Database(dbpath);
+
+db.all("SELECT * FROM mahasiswa ", (err, data) => {
+    if (err) return console.log(data)
+    console.log(data)
+})
