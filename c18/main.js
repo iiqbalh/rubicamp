d7ddb125e4f6049a4ec594b3bfaddc03946c6321
iiqbalh <iqbalhernadi17@gmail.com>
2024-05-15 12:53:30 +0700

    edit

--- c18/main.js
+++ c18/main.js
@@ -5,7 +5,7 @@ const dbpath = path.join(path.resolve(), 'db', 'university.db')
 
 export const db = new sqlite3.Database(dbpath);
 
-db.all("SELECT * FROM mahasiswa ", (err, data) => {
+db.all("SELECT * FROM matakuliah ", (err, data) => {
     if (err) return console.log(data)
     console.log(data)
 })
