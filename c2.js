ca4461d94fdb7681953548680c1ddcac02485e94
iiqbalh <iqbalhernadi17@gmail.com>
2025-04-19 23:53:00 +0700

    c2 done

--- c2.js
+++ c2.js
@@ -1,5 +1,20 @@
 const deretKaskus = n => {
+
+    const arr = [];
+
+    for (let i = 3; i <= n * 3; i += 3) {
+        if (i % 5 === 0 && i % 6 === 0) {
+            arr.push("KASKUS")
+        } else if (i % 5 === 0) {
+            arr.push("KAS")
+        } else if (i % 6 === 0) {
+            arr.push("KUS")
+        } else {
+            arr.push(i)
+        }
+    }
     
+    return arr
 };
 
 console.log(deretKaskus(10));
