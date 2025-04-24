97e9bec6cd10b79e38dda813975f34fed69cb823
iiqbalh <iqbalhernadi17@gmail.com>
2025-04-24 22:14:34 +0700

    done

--- c12/c12.js
+++ c12/c12.js
@@ -1,77 +1,9 @@
-// const txt = process.argv[2];
-
-// if(!txt){
-//     console.log(`Tolong sertakan nama file sebagai inputan soalnya.\nMisalnya 'node solution.js data.json'`)
-//     process.exit();
-// }
-
-// const { readFileSync } = require('node:fs');
-// const hasil = readFileSync(`${txt}`, 'utf-8');
-// const data = JSON.parse(hasil);
-
-// if (txt) {
-//     console.log(`Selamat datang di permainan Tebak-tebakan, kamu akan diberikan pertanyaan dari file ini ${txt}!\nUntuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanya lagi.\n`);
-
-//     const rl = require('readline').createInterface({
-//         input: process.stdin,
-//         output: process.stdout,
-//         prompt: 'Jawaban: '
-//     });
-
-//     let counter = 0;
-//     let count = 1;
-
-//     console.log(`Pertanyaan: ${data[counter].definition}`);
-
-//     rl.prompt();
-
-//     rl.on('line', (line) => {
-//         if (line == data[counter].term) {
-//             console.log(`\nAnda Beruntung!\n`);
-//             count = 1;
-//             counter++;
-
-//             if (counter === data.length) rl.close();
-//             console.log(`Pertanyaan: ${data[counter].definition}`);
-
-//         } else if (line === 'skip') {
-//             data.push(data[counter]);
-//             data.splice(counter, 1);
-
-//             console.log(`\nPertanyaan: ${data[counter].definition}`);
-//             count = 1;
-
-//         } else {
-//             console.log(`\nAnda kurang beruntung! anda telah salah ${count++}, silahkan coba lagi.`);
-//         }
-
-//         rl.prompt();
-
-//     }).on('close', () => {
-//         console.log('Anda berhasil!');
-//         process.exit(0);
-//     });
-// } else {
-//     return error;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
 const text = process.argv[2]
 
 if (!text) {
     console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node c12.js data.json'");
     process.exit();
-} 
+}
 
 const rl = require('readline').createInterface({
     input: process.stdin,
@@ -84,7 +16,7 @@ const data = JSON.parse(path);
 let count = 0;
 let wrong = 1
 
-if(text) {
+if (text) {
     console.log(`Selamat datang di permainan Tebak-tebakan. Kamu akan diberikan pertanyaan dari file ini '${text}'.
 Untuk bermain jawablah dengan jawaban yang sesuai.
 Gunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi.
@@ -97,12 +29,12 @@ Gunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan di
 
     rl.on('line', (line) => {
 
-        if(line === 'skip') {
+        if (line === 'skip') {
             data.push(data[count]);
             data.splice(count, 1);
             console.log(`\npertanyaan: ${data[count].definition}`);
-            
-        }else if (line !== data[count].term) {
+
+        } else if (line !== data[count].term) {
             console.log(`Anda kurang beruntung! Anda telah salah ${wrong} kali, silahkan coba lagi.\n`);
             wrong++;
         } else {
@@ -114,7 +46,7 @@ Gunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan di
         }
         rl.prompt();
     }).on('close', () => {
-        console.log('Hore Anda Menang!\n');
+        console.log('Anda Berhasil!\n');
         process.exit(0);
     });
 } else {
--- c13/c13.js
+++ c13/c13.js
@@ -1,128 +1,128 @@
-const txt = process.argv[2];
-const { readFileSync, writeFileSync } = require('node:fs');
+// const txt = process.argv[2];
+// const { readFileSync, writeFileSync } = require('node:fs');
 
 
-const reading = () => {
-    return JSON.parse(readFileSync('data2.json', 'utf-8'))
-}
+// const reading = () => {
+//     return JSON.parse(readFileSync('data2.json', 'utf-8'))
+// }
 
-const writing = (value) => {
-    writeFileSync('data2.json', JSON.stringify(value), 'utf-8')
-}
+// const writing = (value) => {
+//     writeFileSync('data2.json', JSON.stringify(value), 'utf-8')
+// }
 
-const data = reading();
+// const data = reading();
 
-switch (txt) {
-    case "list":
-        console.log(`daftar kerjaan:`);
-        data.forEach((item, index) => {
-            console.log(`${index + 1}. [${item.complete ? 'X' : ' '}] ${item.title}`);
-        });
-        break;
+// switch (txt) {
+//     case "list":
+//         console.log(`daftar kerjaan:`);
+//         data.forEach((item, index) => {
+//             console.log(`${index + 1}. [${item.complete ? 'X' : ' '}] ${item.title}`);
+//         });
+//         break;
 
-    case "task":
-        const txt1 = process.argv[3] - 1;
-        console.log(`daftar kerjaan task ${txt1 + 1}: \ntask content: ${data[txt1].title} \ncomplete: ${data[txt1].complete ? 'done' : 'not yet'}\ntags: ${data[txt1].tags}`);
-        break;
+//     case "task":
+//         const txt1 = process.argv[3] - 1;
+//         console.log(`daftar kerjaan task ${txt1 + 1}: \ntask content: ${data[txt1].title} \ncomplete: ${data[txt1].complete ? 'done' : 'not yet'}\ntags: ${data[txt1].tags}`);
+//         break;
 
-    case "add":
-        const save = process.argv.splice(3, 10).join(" ");
-        data.push({title: save, complete: false, tags: []});
-        writing(data);
-        console.log(`"${save}" telah ditambahkan.`);
-        break;
+//     case "add":
+//         const save = process.argv.splice(3, 10).join(" ");
+//         data.push({title: save, complete: false, tags: []});
+//         writing(data);
+//         console.log(`"${save}" telah ditambahkan.`);
+//         break;
 
-    case "delete":
-        const txt2 = process.argv[3] - 1;
-        console.log(`"${data[txt2].title}" telah dihapus dari daftar`);
-        data.splice(txt2, 1);
-        writing(data);
-        break;
+//     case "delete":
+//         const txt2 = process.argv[3] - 1;
+//         console.log(`"${data[txt2].title}" telah dihapus dari daftar`);
+//         data.splice(txt2, 1);
+//         writing(data);
+//         break;
 
-    case "complete":
-        const txt3 = process.argv[3] - 1;
-        data[txt3].complete = true;
-        writing(data);
-        console.log(`"${data[txt3].title}" telah selesai`);
-        break;
+//     case "complete":
+//         const txt3 = process.argv[3] - 1;
+//         data[txt3].complete = true;
+//         writing(data);
+//         console.log(`"${data[txt3].title}" telah selesai`);
+//         break;
 
-    case "uncomplete":
-        const txt4 = process.argv[3] - 1;
-        data[txt4].complete = false;
-        writing(data);
-        console.log(`"${data[txt4].title}" status selesai dibatalkan`);
-        break;
+//     case "uncomplete":
+//         const txt4 = process.argv[3] - 1;
+//         data[txt4].complete = false;
+//         writing(data);
+//         console.log(`"${data[txt4].title}" status selesai dibatalkan`);
+//         break;
 
-    case "list:outstanding":
-        const txt5 = process.argv[3];
-        console.log(`daftar pekerjaan:`);
-        if (txt5 === 'asc') {
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].complete === false) {
-                    console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
-                }
-            }
-        } else {
-            for (let i = data.length - 1; i >= 0; i--) {
-                if (data[i].complete === false) {
-                    console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
-                }
-            }
-        };
-        break;
+//     case "list:outstanding":
+//         const txt5 = process.argv[3];
+//         console.log(`daftar pekerjaan:`);
+//         if (txt5 === 'asc') {
+//             for (let i = 0; i < data.length; i++) {
+//                 if (data[i].complete === false) {
+//                     console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
+//                 }
+//             }
+//         } else {
+//             for (let i = data.length - 1; i >= 0; i--) {
+//                 if (data[i].complete === false) {
+//                     console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
+//                 }
+//             }
+//         };
+//         break;
 
-    case "list:completed":
-        const txt6 = process.argv[3];
-        console.log(`daftar pekerjaan:`);
-        if (txt6 === 'asc') {
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].complete === true) {
-                    console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
-                }
-            }
-        } else {
-            for (let i = data.length - 1; i >= 0; i--) {
-                if (data[i].complete === true) {
-                    console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
-                }
-            }
-        };
-        break;
+//     case "list:completed":
+//         const txt6 = process.argv[3];
+//         console.log(`daftar pekerjaan:`);
+//         if (txt6 === 'asc') {
+//             for (let i = 0; i < data.length; i++) {
+//                 if (data[i].complete === true) {
+//                     console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
+//                 }
+//             }
+//         } else {
+//             for (let i = data.length - 1; i >= 0; i--) {
+//                 if (data[i].complete === true) {
+//                     console.log(`${i + 1}. [${data[i].complete ? 'X' : ''}] ${data[i].title}`);
+//                 }
+//             }
+//         };
+//         break;
 
-    case "tag":
-        const txt7 = process.argv[3] - 1;
-        const txt8 = process.argv.splice(4, 10);
-        txt8.forEach(items => data[txt7].tags.push(items))
-        writing(data)
-        console.log(`Tag '${txt8}' telah ditambahkan ke daftar '${data[txt7].title}'`)
-        break;
+//     case "tag":
+//         const txt7 = process.argv[3] - 1;
+//         const txt8 = process.argv.splice(4, 10);
+//         txt8.forEach(items => data[txt7].tags.push(items))
+//         writing(data)
+//         console.log(`Tag '${txt8}' telah ditambahkan ke daftar '${data[txt7].title}'`)
+//         break;
 
-    default:
-        const txt9 = process.argv[2];
-        if (txt9) {
-            console.log('daftar pekerjaan:')
-            result = txt9.split(":");
-            data.forEach((items, index) => {
-                if (items.tags.includes(result[1])) {
-                    console.log(`${index + 1}. [${items.complete ? 'X' : ''}] ${items.title}`);
-                }
-            })
-        } else {
-            console.log(`
->>> JS TODO <<<
-$ node todo.js <command>
-$ node todo.js list
-$ node todo.js task <task_id>
-$ node todo.js add <task_content>
-$ node todo.js delete <task_id>
-$ node todo.js complete <task_id>
-$ node todo.js uncomplete <task_id>
-$ node todo.js list:outstanding asc|desc
-$ node todo.js list:completed asc|desc
-$ node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
-$ node todo.js filter: <tag_name>
-         `);
-        }
-        break;
-}
+//     default:
+//         const txt9 = process.argv[2];
+//         if (txt9) {
+//             console.log('daftar pekerjaan:')
+//             result = txt9.split(":");
+//             data.forEach((items, index) => {
+//                 if (items.tags.includes(result[1])) {
+//                     console.log(`${index + 1}. [${items.complete ? 'X' : ''}] ${items.title}`);
+//                 }
+//             })
+//         } else {
+//             console.log(`
+// >>> JS TODO <<<
+// $ node todo.js <command>
+// $ node todo.js list
+// $ node todo.js task <task_id>
+// $ node todo.js add <task_content>
+// $ node todo.js delete <task_id>
+// $ node todo.js complete <task_id>
+// $ node todo.js uncomplete <task_id>
+// $ node todo.js list:outstanding asc|desc
+// $ node todo.js list:completed asc|desc
+// $ node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
+// $ node todo.js filter: <tag_name>
+//          `);
+//         }
+//         break;
+// }
 
